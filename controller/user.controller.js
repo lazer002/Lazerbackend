e0aa7c1ef639d875f53704973850414223d759a2
ajitkumar12 <ajitkumar@nicobar.com>
2025-07-24 15:34:11 +0530

    setup

---
+++ controller/user.controller.js
@@ -0,0 +1,21 @@
+const User = require('../models/user.model');
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
