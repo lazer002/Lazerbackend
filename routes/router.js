f7527d306c86fc6e7a1ecfa03428760e63d92390
ajit.none <129359686+lazer002@users.noreply.github.com>
2025-08-16 23:15:13 +0530

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user.controller');
-const { createUserData } = require("../controllers/userData.controller");
+const { createUserData } = require("../controller/userData.controller");
 router.get('/', userController.getUsers);
 router.post('/', userController.createUser);
 router.post("/user-data", createUserData);
