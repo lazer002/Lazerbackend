8ffd2ab46bae2ba97a4fe251675db43c1766b298
ajit.none <ajlazer002@gmail.com>
2025-08-17 17:22:51 +0530

    api changesss

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controller/user.controller');
 const { createUserData } = require("../controller/userData.controller");
-router.get('/', userController.getUsers);
-router.post('/', userController.createUser);
+
+router.get("/", (req,res)=>{
+    res.send("Hello World");
+});
 router.post("/user-data", createUserData);
 
 module.exports = router;
